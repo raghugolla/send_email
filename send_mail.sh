7ca14377b063de715d878bbcae4cd7af8e7aa378
raghu golla <raghugolla.22@gmail.com>
2020-12-16 23:22:33 +0530

    fix: adding python

--- send_mail.sh
+++ send_mail.sh
@@ -1,5 +1,5 @@
 mkdir -f "$HOME/email"
 /jes/dsee6/dsrk6/bin/ldapsearch -h p4033-adm -p 3636 -Z -P /jes/dsee6/slapd-cert8.db -D "cn=directory manager" -w - -b "ou=system accounts,dc=cms,dc=hhs,dc=gov" -s sub uid=* pwdchangedtime pwdPolicySubentry uid cn sn nsaccountlock mail nsaccountlock pwdExpirationWarned passwordexpirationtime > $HOME/email/customer_records.ldif
 sleep 300
-$HOME/email/main.py -fp $HOME/email/customer_records.ldif
+python $HOME/email/main.py -fp $HOME/email/customer_records.ldif
 rm -rf $HOME/email/customer_records.ldif
