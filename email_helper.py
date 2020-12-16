857a950a10547b39a8a070bd0daca010e40328e7
raghu golla <raghugolla.22@gmail.com>
2020-12-16 22:35:01 +0530

    adding script to send email from ldif file

---
+++ email_helper.py
@@ -0,0 +1,64 @@
+import smtplib, ssl
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from typing import Dict, List
+
+SENDER_EMAIL = "1206chandrasekhar@gmail.com"
+SENDER_PASSWORD = "Newyear@2019"
+
+
+def get_body(
+    receiver_email: str,
+    data: List[Dict] = None,
+    subject: str = "Email Reset Notification",
+):
+    message = MIMEMultipart("alternative")
+    message["Subject"] = subject
+    message["From"] = SENDER_EMAIL
+    message["To"] = receiver_email
+    if data:
+        text = f"""\
+                       Hi,
+                       Please find the data {data}"""
+
+        html = """<!DOCTYPE html><title></title><p>Hello,<p>Following is the message 
+        <ul>%(somelist)s</ul><p>Thank you. """ % dict(
+            somelist="\n".join(["<li> " + str(item) for item in data])
+        )
+        # html = f"""\
+        #         <html>
+        #           <body>
+        #             <p>Hi,<br>
+        #                Please find the data {data}<br>
+        #             </p>
+        #           </body>
+        #         </html>
+        #         """
+    else:
+        text = """\
+                Hi,
+                Your password is about to expire, please update your password"""
+        html = """\
+        <html>
+          <body>
+            <p>Hi,<br>
+               Your password is about to expire, please update your password<br>
+            </p>
+          </body>
+        </html>
+        """
+
+    part1 = MIMEText(text, "plain")
+    part2 = MIMEText(html, "html")
+
+    message.attach(part1)
+    message.attach(part2)
+    return message
+
+
+def send_email(receiver_email: str, data: List = None, subject: str = None):
+    message = get_body(receiver_email=receiver_email, data=data, subject=subject)
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+        server.login(SENDER_EMAIL, SENDER_PASSWORD)
+        server.sendmail(SENDER_EMAIL, receiver_email, message.as_string())
---
+++ main.py
@@ -0,0 +1,84 @@
+import os
+import re
+import sys
+import argparse
+import time
+
+from ldif3 import LDIFParser
+
+from email_helper import send_email
+from dateutil import parser as datetime_parser
+from datetime import datetime, timezone
+
+MASTER_MAIL = "1206chandrasekhar@gmail.com"
+THRESHLOLD_DAYS = 2
+
+
+def is_system_account_password(value) -> bool:
+    return (
+        True
+        if value and re.search(r"System Account Password Policy", value[0])
+        else False
+    )
+
+
+def is_120_days_expire_password(value) -> bool:
+    return True if value and re.search(r"120days", value[0]) else False
+
+
+def send_mail(record, days_diff: int) -> str:
+    password_changed_time = record.get("pwdchangedtime", None)
+    if not password_changed_time:
+        return "password changed time not available"
+    password_changed_time = datetime_parser.parse(password_changed_time[0])
+    now = datetime.now(timezone.utc)
+    if (now - password_changed_time).days > days_diff - THRESHLOLD_DAYS:
+        for mail in record.get("mail"):
+            send_email(receiver_email=mail, subject="Email Reset Notification")
+        return "Successfully Send email"
+    return "Not needed"
+
+
+def parse_and_send_email(record) -> str:
+    password_policy_sub_entry_type = record.get("pwdPolicySubentry")
+    if is_system_account_password(password_policy_sub_entry_type):
+        return "SYSTEM PASSWORD (No email sent)"
+    elif is_120_days_expire_password(password_policy_sub_entry_type):
+        return send_mail(record, 120)
+    else:
+        return send_mail(record, 60)
+
+
+def read_file_and_send_email(parser):
+    sent_list, error = [], []
+    for dn, entry in parser.parse():
+        try:
+            if not entry.get("mail"):
+                pass
+            message = parse_and_send_email(entry)
+            sent_list.append({",".join(entry.get("mail")): message})
+            time.sleep(10)
+        except Exception as e:
+            error.append(e)
+
+    send_email(MASTER_MAIL, data=sent_list, subject="Report")
+    time.sleep(10)
+    if error:
+        send_email(MASTER_MAIL, data=error, subject="Failed To Send Email")
+
+
+def create_arg_parser():
+    parser = argparse.ArgumentParser(description="Description of send mail script")
+    parser.add_argument(
+        "-inputDirectory", "-fp", help="Path to the ldif file directory."
+    )
+
+    return parser
+
+
+if __name__ == "__main__":
+    arg_parser = create_arg_parser()
+    parsed_args = arg_parser.parse_args(sys.argv[1:])
+    if os.path.exists(parsed_args.inputDirectory):
+        parser = LDIFParser(open(parsed_args.inputDirectory, "rb"))
+        read_file_and_send_email(parser=parser)
